054604d2bd764203948659f920fcc01bb861dd9c
Xinge Xu <xingexu1107@gmail.com>
2024-02-10 15:59:06 -0500

    fixed add button(delete button not working)

--- src/main/java/com/fbla/parters/PartersApplication.java
+++ src/main/java/com/fbla/parters/PartersApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PartersApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)     {
 		SpringApplication.run(PartersApplication.class, args);
 	}
 
