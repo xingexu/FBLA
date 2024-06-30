d39724941c291c12482c32bf1af672f734fd35d9
Xinge Xu <xingexu1107@gmail.com>
2024-06-30 00:51:26 -0400

    completed layout and finishing touches

--- src/main/java/com/fbla/parters/security/SecurityConfig.java
+++ src/main/java/com/fbla/parters/security/SecurityConfig.java
@@ -8,38 +8,44 @@ import org.springframework.security.authentication.DefaultAuthenticationEventPub
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated())
-            .httpBasic(Customizer.withDefaults()) 
-            .formLogin(form -> form
-                .loginPage("/login")  
-                .permitAll()) 
-            .rememberMe(Customizer.withDefaults());
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/partners/{id}/delete")).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/partners")).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/addpartner")).hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/savepartner")).hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"))
+                .rememberMe(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
-
     @Bean
     public UserDetailsService userDetailsService() {
-        
+
         return new InMemoryUserDetailsManager(
                 User.withUsername("admin").password("{noop}adminpassword1234").roles("ADMIN").build(),
                 User.withUsername("user").password("{noop}userpassword1234").roles("USER").build(),
-                User.withUsername("xinge").password("{noop}demopassword1234").roles("USER","ADMIN").build());
+                User.withUsername("xinge").password("{noop}demopassword1234").roles("USER", "ADMIN").build());
     }
 
     @Bean
